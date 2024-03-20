8d21ddbbb6c7b746e02e49d6f4a6ba2a4ed07c1d
Ayushpatel1803 <155693412+Ayushpatel1803@users.noreply.github.com>
2024-03-19 18:50:42 -0600

    Add files via upload

---
+++ patela89_Assignment 2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Eventotal = 0;
+            int Oddtotal = 0;
+
+           
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine($"{i} is even number");
+                     Eventotal += i; 
+                }
+                else
+                {
+                    Console.WriteLine($"{i} is odd number");
+                     Oddtotal += i; 
+                }
+            }
+
+            
+            Console.WriteLine($"Even number's total is: {Eventotal}");
+            Console.WriteLine($"Odd number's Toatl is: {Oddtotal}");
+
+          
+            Console.ReadLine();
+        }
+    }
+
